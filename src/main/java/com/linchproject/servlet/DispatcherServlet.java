c5269f8f10ebffd546cdded629263ede1c5e619e
Georg Schmidl <georg.schmidl@vicox.net>
2014-02-12 22:54:02 +0100

    added handling of binary result

--- src/main/java/com/linchproject/servlet/DispatcherServlet.java
+++ src/main/java/com/linchproject/servlet/DispatcherServlet.java
@@ -1,6 +1,10 @@
 package com.linchproject.servlet;
 
-import com.linchproject.core.*;
+import com.linchproject.core.Container;
+import com.linchproject.core.Invoker;
+import com.linchproject.core.Result;
+import com.linchproject.core.Route;
+import com.linchproject.core.results.Binary;
 import com.linchproject.core.results.Error;
 import com.linchproject.core.results.Redirect;
 import com.linchproject.core.results.Success;
@@ -10,9 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 import java.util.Enumeration;
 import java.util.Properties;
 
@@ -79,13 +81,17 @@ public class DispatcherServlet extends HttpServlet {
     protected Route getRoute(HttpServletRequest req) {
         Route route = new ServletRoute(req.getContextPath());
 
-        String uri = req.getRequestURI().substring(req.getContextPath().length());
+        String uri = req.getRequestURI().substring(req.getContextPath().length() + 1);
         String[] uriSplit = uri.split("/");
         if (uriSplit.length > 0 && uriSplit[0].length() > 0) {
             route.setController(uriSplit[0]);
         }
         if (uriSplit.length > 1 && uriSplit[1].length() > 0) {
             route.setAction(uriSplit[1]);
+        }
+
+        if (uriSplit.length > 2 && uriSplit[2].length() > 0) {
+            route.setTail(uriSplit[2]);
         }
 
         route.setParameterMap(req.getParameterMap());
@@ -99,6 +105,18 @@ public class DispatcherServlet extends HttpServlet {
             resp.setContentType("text/html;charset=utf-8");
             resp.setStatus(HttpServletResponse.SC_OK);
             resp.getWriter().println(success.getContent());
+
+        } else if (result instanceof Binary) {
+            InputStream inputStream = ((Binary) result).getInputStream();
+            OutputStream outputStream = resp.getOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+            inputStream.close();
 
         } else if (result instanceof Redirect) {
             Redirect redirect = (Redirect) result;
