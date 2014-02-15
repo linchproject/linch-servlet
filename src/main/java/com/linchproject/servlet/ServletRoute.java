4c5a9ebff17c270e3d657e71a66a94f1a96e1f00
Georg Schmidl <georg.schmidl@vicox.net>
2014-02-15 16:35:11 +0100

    build queryString from parameterMap

--- src/main/java/com/linchproject/servlet/ServletRoute.java
+++ src/main/java/com/linchproject/servlet/ServletRoute.java
@@ -3,6 +3,7 @@ package com.linchproject.servlet;
 import com.linchproject.core.Route;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @author Georg Schmidl
@@ -17,10 +18,26 @@ public class ServletRoute extends Route {
         this.request = request;
 
         String path = request.getRequestURI().substring(request.getContextPath().length() + 1);
-        if (request.getQueryString() != null) {
-            path += "?" + request.getQueryString();
+        if (!request.getParameterMap().isEmpty()) {
+            path += "?" + getQueryString(request.getParameterMap());
         }
         setPath(path);
+    }
+
+    private String getQueryString(Map<String, String[]> map) {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String[]> entry : map.entrySet()) {
+            String key = entry.getKey();
+            for (String value : entry.getValue()) {
+                if (sb.length() > 0) {
+                    sb.append("&");
+                }
+                sb.append(key);
+                sb.append("=");
+                sb.append(value);
+            }
+        }
+        return sb.toString();
     }
 
     @Override
