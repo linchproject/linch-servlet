6cbd77d4abe7a9eebdc01a4e38296a5ece6b88c7
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-08 14:34:35 +0100

    removed dependency to linch-apps; loading linch-app-properties, linch-db-properties and linch-components-properties

--- src/main/java/com/linchproject/servlet/DispatcherServlet.java
+++ src/main/java/com/linchproject/servlet/DispatcherServlet.java
@@ -1,7 +1,5 @@
 package com.linchproject.servlet;
 
-import com.linchproject.apps.App;
-import com.linchproject.apps.AppRegistry;
 import com.linchproject.core.Injector;
 import com.linchproject.core.Invoker;
 import com.linchproject.core.Result;
@@ -17,35 +15,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.beans.PropertyVetoException;
 import java.io.IOException;
-import java.util.Map;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * @author Georg Schmidl
  */
 public class DispatcherServlet extends HttpServlet {
 
-    private static final String APP_PROPERTIES = "app.properties";
     private static final String CONTROLLER_SUB_PACKAGE = "controllers";
 
     private ClassLoader classLoader;
-    private AppRegistry appRegistry;
     private ComboPooledDataSource dataSource;
-
-    private App mainApp;
 
     private Container container;
     private InvokerWrapper invokerWrapper;
 
+    private Properties appProperties;
+    private Properties dbProperties;
+    private List<Properties> componentPropertiesList;
+
     @Override
     public void init() throws ServletException {
         classLoader = getClass().getClassLoader();
-        appRegistry = new AppRegistry();
-        appRegistry.loadFromClassPath();
 
-        try {
-            mainApp = App.load(classLoader, APP_PROPERTIES);
-        } catch (IOException e) {
-            throw new ServletException("missing " + APP_PROPERTIES, e);
+        loadAppProperties();
+        loadDbProperties();
+        loadComponentProperties();
+
+        if (appProperties == null) {
+            throw new ServletException("linch-app.properties is missing");
         }
 
         dataSource = createDataSource();
@@ -72,7 +76,7 @@ public class DispatcherServlet extends HttpServlet {
         ServletThreadLocal.setRequest(request);
         ServletThreadLocal.setResponse(response);
 
-        String appPackage = mainApp.get("package");
+        String appPackage = appProperties.getProperty("package");
         String controllersPackage = appPackage != null? appPackage + "." + CONTROLLER_SUB_PACKAGE : CONTROLLER_SUB_PACKAGE;
 
         Route route = new ServletRoute(request);
@@ -89,35 +93,45 @@ public class DispatcherServlet extends HttpServlet {
     private Container createContainer(ClassLoader classLoader) throws ServletException {
         Container container = new Container();
 
-        container.add("app", mainApp);
         container.add("dataSource", dataSource);
         container.add("classLoader", classLoader);
         container.add("sessionService", ServletSessionService.class);
         container.add("cookieService", ServletCookieService.class);
 
-        for (App app : appRegistry.getApps()) {
-            for (Map.Entry<String, String> entry : app.getMap("component.").entrySet()) {
-                Class<?> componentClass;
-                try {
-                    componentClass = classLoader.loadClass(entry.getValue());
-                } catch (ClassNotFoundException e) {
-                    throw new ServletException("class not found for component " + entry.getKey(), e);
+        if (componentPropertiesList != null) {
+            for (Properties componentProperties : componentPropertiesList) {
+                Enumeration<?> enumeration = componentProperties.propertyNames();
+                while (enumeration.hasMoreElements()) {
+                    String componentName = (String) enumeration.nextElement();
+                    String componentClassName = componentProperties.getProperty(componentName);
+
+                    Class<?> componentClass;
+                    try {
+                        componentClass = classLoader.loadClass(componentClassName);
+                    } catch (ClassNotFoundException e) {
+                        throw new ServletException("error loading class for component " + componentName, e);
+                    }
+                    container.add(componentName, componentClass);
                 }
-                container.add(entry.getKey(), componentClass);
             }
         }
         return container;
     }
 
     private ComboPooledDataSource createDataSource() throws ServletException {
-        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
-        try {
-            comboPooledDataSource.setDriverClass(mainApp.get("jdbc.driver"));
-            comboPooledDataSource.setJdbcUrl(mainApp.get("jdbc.url"));
-            comboPooledDataSource.setUser(mainApp.get("jdbc.user"));
-            comboPooledDataSource.setPassword(mainApp.get("jdbc.password"));
-        } catch (PropertyVetoException e) {
-            throw new ServletException("error creating datasource", e);
+        ComboPooledDataSource comboPooledDataSource = null;
+
+        if (dbProperties != null) {
+            comboPooledDataSource = new ComboPooledDataSource();
+            try {
+                comboPooledDataSource.setDriverClass(dbProperties.getProperty("jdbc.driver"));
+                comboPooledDataSource.setJdbcUrl(dbProperties.getProperty("jdbc.url"));
+                comboPooledDataSource.setUser(dbProperties.getProperty("jdbc.user"));
+                comboPooledDataSource.setPassword(dbProperties.getProperty("jdbc.password"));
+            } catch (PropertyVetoException e) {
+                throw new ServletException("error creating datasource", e);
+            }
+
         }
         return comboPooledDataSource;
     }
@@ -151,7 +165,7 @@ public class DispatcherServlet extends HttpServlet {
 
             Result result = invoker.invoke(route);
 
-            if (!(result instanceof com.linchproject.core.results.Error)) {
+            if (!(result instanceof Error)) {
                 container.commit();
             } else {
                 container.rollback();
@@ -165,7 +179,7 @@ public class DispatcherServlet extends HttpServlet {
 
         @Override
         public Result invoke(Route route) throws ServletException {
-            ClassLoader dynamicClassLoader = new DynamicClassLoader(classLoader, mainApp.get("package"));
+            ClassLoader dynamicClassLoader = new DynamicClassLoader(classLoader, appProperties.getProperty("package"));
             Container container = createContainer(dynamicClassLoader);
             Invoker invoker = createInvoker(dynamicClassLoader, container);
 
@@ -191,5 +205,40 @@ public class DispatcherServlet extends HttpServlet {
                 container.inject(object);
             }
         });
+    }
+
+    private void loadAppProperties() throws ServletException {
+        appProperties = loadProperties(classLoader, "linch-app.properties");
+    }
+
+    private void loadDbProperties() throws ServletException {
+        dbProperties = loadProperties(classLoader, "linch-db.properties");
+    }
+
+    private void loadComponentProperties() throws ServletException {
+        componentPropertiesList = new ArrayList<Properties>();
+
+        URLClassLoader contextClassLoader = (URLClassLoader)Thread.currentThread().getContextClassLoader();
+        for (URL url : contextClassLoader.getURLs()) {
+            URLClassLoader urlClassLoader = new URLClassLoader(new URL[] { url });
+            Properties componentProperties = loadProperties(urlClassLoader, "linch-components.properties");
+            if (componentProperties != null) {
+                componentPropertiesList.add(componentProperties);
+            }
+        }
+    }
+
+    private Properties loadProperties(ClassLoader classLoader, String propertiesFileName) throws ServletException {
+        Properties properties = null;
+        InputStream inputStream = classLoader.getResourceAsStream(propertiesFileName);
+        if (inputStream != null) {
+            try {
+                properties = new Properties();
+                properties.load(inputStream);
+            } catch (IOException e) {
+                throw new ServletException(e.getMessage(), e);
+            }
+        }
+        return properties;
     }
 }
